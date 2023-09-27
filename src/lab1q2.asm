01768d640f16564aee43c1313e48994324604578
Chloe <136621608+cabbruzz@users.noreply.github.com>
2023-09-27 21:11:27 +0000

    input some message here

---
+++ src/lab1q2.asm
@@ -0,0 +1,55 @@
+
+
+; COMP2660 Lab 1 Section 2 Question 2
+
+; Name: Chloe Abbruzzese
+; Date: 09/22/2023
+; Due Date: 09/26/2023 @ 11:59 PM
+; ID: 110105377
+; Description: Assembly code that adds two hexadecimal numbers (2000h + 3000h) and then subtracts 1000h.
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
+
+; these two lines are only necessary if you're not using Visual Studio
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+
+.data
+    
+	; data declarations go here
+
+    Var1 DWORD 30000h
+    Var2 DWORD 10000h
+    Result DWORD ?
+
+
+.code
+main PROC
+	
+	; code goes here
+
+    ; Load Var1 into EAX
+    mov eax, Var1
+
+    ; Add Var2 to EAX
+    sub eax, Var2
+
+    ; Store the result in Result
+    mov Result, eax
+
+    ; displays registers in console
+	call DumpRegs 
+
+	exit
+
+main ENDP
+END main
+
+; Questions:
+; 1. What is the final value of the EAX register?
+;       EAX = 20000h
+; 2. What will be different if we have a decimal representation?
+;       it would be converted into hexadecimal
+; 3. Write a full Assembly program to verify your answer in (1)
+;       = code above
